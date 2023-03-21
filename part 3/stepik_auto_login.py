8dd53f990efbc4812040484ce9acbd6d07aa3c77
евгений <79500420830@yandex.ru>
2023-03-21 20:15:11 +0100

    task_3_6.py is finished

--- part 3/stepik_auto_login.py
+++ part 3/stepik_auto_login.py
@@ -1,7 +1,6 @@
 import time
 
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -39,6 +38,3 @@ def test_guest_should_see_login_link(browser):
     )
     button.click()
     time.sleep(5)
-
-
-
