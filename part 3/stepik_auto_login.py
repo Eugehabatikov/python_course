61b82d4a3d7bc20ed50a09f49099dbc810e261dc
евгений <79500420830@yandex.ru>
2023-03-21 20:32:57 +0300

    stepik_auto_login.py complete

---
+++ part 3/stepik_auto_login.py
@@ -0,0 +1,44 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+def test_guest_should_see_login_link(browser):
+    login = input("Введите свой логин: ")
+    password = input("Введите свой пароль: ")
+    link = f"https://stepik.org/lesson/236895/step/1"
+    browser.get(link)
+
+    # click
+    button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.ID, "ember33"))
+    )
+    button.click()
+
+    # input
+    input1 = browser.find_element(By.ID, "id_login_email")
+    input1.send_keys(login)
+    input1 = browser.find_element(By.ID, "id_login_password")
+    input1.send_keys(password)
+
+    # click
+    button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "sign-form__btn"))
+    )
+    button.click()
+    time.sleep(5)
+
+
+
